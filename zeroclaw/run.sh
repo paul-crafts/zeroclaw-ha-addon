859ab4feecb8bf3afa7d7000785ce45d214b8629
paul-crafts <paul-crafts@users.noreply.github.com>
2026-04-08 10:51:19 +0200

    Clean up redundant UI options and bump version to v0.1.1

--- zeroclaw/run.sh
+++ zeroclaw/run.sh
@@ -5,11 +5,5 @@ mkdir -p /data/zeroclaw
 
 bashio::log.info "Initializing ZeroClaw Add-on..."
 
-# Optionally, you could extract options from HA UI configuration here, e.g.:
-# PROVIDER=$(bashio::config 'provider')
-# MODEL=$(bashio::config 'model')
-# GATEWAY_PORT=$(bashio::config 'gateway_port')
-# bashio::log.info "Configured to use provider: ${PROVIDER} and model: ${MODEL}"
-
 bashio::log.info "Starting ZeroClaw daemon..."
 exec /usr/local/bin/zeroclaw --config-dir /data/zeroclaw daemon
