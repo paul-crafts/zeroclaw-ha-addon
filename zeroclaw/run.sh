ad47e28ed66e0d6ee193dec04a01057020018ae5
paul-crafts <paul-crafts@users.noreply.github.com>
2026-04-16 14:49:02 +0200

    Move config to /config/zeroclaw and bump version to 0.1.7

--- zeroclaw/run.sh
+++ zeroclaw/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bashio
 
 # Create data directory for persistent configuration if it doesn't exist
-mkdir -p /data/zeroclaw
+mkdir -p /config/zeroclaw
 
 bashio::log.info "Initializing ZeroClaw Add-on..."
 
 bashio::log.info "Starting ZeroClaw daemon..."
-exec /usr/local/bin/zeroclaw --config-dir /data/zeroclaw daemon --host 0.0.0.0
+exec /usr/local/bin/zeroclaw --config-dir /config/zeroclaw daemon --host 0.0.0.0
